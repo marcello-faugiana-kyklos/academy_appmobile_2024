10653ee61e864b6137fb0b39bc0ec8ca050fc537
Marcello <marcello.faugiana@kyklos-group.com>
2024-03-13 16:33:58 +0100

    Console app net8 vs net48

---
+++ TestConsoleApp/TestConsoleAppNet48/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleAppNet48
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            return 0;
+        }
+    }
+}
---
+++ TestConsoleApp/TestConsoleAppNet8/Program.cs
@@ -0,0 +1,33 @@
+﻿if (args.Length == 0)
+{
+    Console.WriteLine("Please specify a file name!");
+    Environment.Exit(1);
+}
+
+string fileName = args[0];
+
+if (!File.Exists(fileName))
+{
+    Console.WriteLine($"The file {fileName} does not exist");
+    Environment.Exit(1);
+}
+
+string[] lines = File.ReadAllLines(fileName);
+
+int n = lines.Length;
+
+if (args.Length >= 2)
+{
+    n = int.Parse(args[1]);
+}
+
+if (n < 0)
+{
+    Console.WriteLine($"The length specified is not valid");
+    Environment.Exit(1);
+}
+
+Console.WriteLine($"Your file contains {lines.Length} lines!");
+
+foreach (string line in lines.Take(n))
+    Console.WriteLine(line);
