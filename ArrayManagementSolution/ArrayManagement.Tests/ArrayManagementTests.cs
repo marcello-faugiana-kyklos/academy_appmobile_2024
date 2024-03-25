563aa743f28e2f7d6fa316ffa6862795a2d9eda0
Marcello <marcello.faugiana@kyklos-group.com>
2024-03-25 12:58:18 +0100

    Completed ArrayManagement implementations and tests

--- ArrayManagementSolution/ArrayManagement.Tests/ArrayManagementTests.cs
+++ ArrayManagementSolution/ArrayManagement.Tests/ArrayManagementTests.cs
@@ -1,35 +1,66 @@
+using FluentAssertions;
+using FluentAssertions.Specialized;
+
 namespace ArrayManagement.Tests;
 
 public class ArrayManagementTests
 {
     [Fact]
-    public void Test1()
+    public void FindIndexOfMin_with_indexes_greater_than_array_length_should_throw_IndexOutOfRangeException()
     {
-        int[] array = [1, 2, 3];
-        uint indexOfMin = ArraySorter.FindIndexOfMin(array, 0, 2);
-        Assert.Equal(0u, indexOfMin);
+        Action action = 
+            () => 
+            {
+                int[] array = [3, 2, 1];
+                uint indexOfMin = ArraySorter.FindIndexOfMin(array, 10, 20);
+            };
+
+        action.Should().Throw<IndexOutOfRangeException>();
     }
 
-    [Fact]
-    public void Test2()
+
+    [Theory]
+    [InlineData(new int[] { 1, 2, 5, -1, 7 }, 0, 3, 3)]
+    [InlineData(new int[] { 1, 2, 5, -1, 7 }, 2, 4, 3)]
+    [InlineData(new int[] { 1, 2, 5, -1, 7 }, 0, 1, 0)]
+    [InlineData(new int[] { -1, -2, -5, -1, 7 }, 1, 4, 2)]
+    [InlineData(new int[] { -1, 2, 3, -8, 5 }, 2, 3, 3)]
+    [InlineData(new int[] { -1, 2, 3, -8, 5 }, 0, 4, 3)]
+    [InlineData(new int[] { -1, 2, 3, -8, 5 }, 0, 2, 0)]
+    public void FindIndexOfMin_should_work(int[] array, uint indexFrom, uint indexTo, uint indexOfMin)
     {
-        try
-        {
-            int[] array = [3, 2, 1];
-            uint indexOfMin = ArraySorter.FindIndexOfMin(array, 10, 20);
-            Assert.True(false);
-        }
-        catch (IndexOutOfRangeException)
-        {
-            Assert.True(true);
-        }  
+        uint actualIndexOfMin = ArraySorter.FindIndexOfMin(array, indexFrom, indexTo);
+        actualIndexOfMin.Should().Be(indexOfMin);
     }
 
-    [Fact]
-    public void Test3()
+
+    [Theory]
+    [InlineData(new int[] { 1, 2, 5, -1 }, 0, 3)]
+    [InlineData(new int[] { 1, 2, 5, -1 }, 1, 2)]
+    [InlineData(new int[] { 1, 2, 5, -1 }, 3, 3)]
+    public void SwapElements_should_work(int[] array, uint i, uint j)
     {
-        int[] array = [-1, 2, 3, -8, 5];
-        uint indexOfMin = ArraySorter.FindIndexOfMin(array, 2, 3);
-        Assert.Equal(3u, indexOfMin);
+        int valueAtI = array[i];
+        int valueAtJ = array[j];
+
+        ArraySorter.SwapElements(array, i, j);
+
+        valueAtI.Should().Be(array[j]);
+        valueAtJ.Should().Be(array[i]);
+    }
+
+    [Theory]
+    [InlineData(new int[] { 1, 2, 5, -1 })]
+    [InlineData(new int[] { 4, 8, 1, 2, 5, -1 })]
+    [InlineData(new int[] { 1, 2, 3, 4, 4, 5, 6 })]
+    public void SortArray_should_work(int[] array)
+    {
+        var sortedArray = array.OrderBy(element => element).ToArray();
+
+        ArraySorter.SortArray(array);
+
+        array.Should().Equal(sortedArray);
+        
+        //array.Should().BeInAscendingOrder();
     }
 }
--- ArrayManagementSolution/ArrayManagement/ArraySorter.cs
+++ ArrayManagementSolution/ArrayManagement/ArraySorter.cs
@@ -15,4 +15,21 @@ public static class ArraySorter
         }
         return indexOfMin;
     }
+
+    public static void SwapElements(int[] array, uint i, uint j)
+    {
+        int valueInPosI = array[i];
+        array[i] = array[j];
+        array[j] = valueInPosI;
+    }
+
+    public static void SortArray(int[] array)
+    {
+        for (uint i = 0; i < array.Length - 1; i++) 
+        {
+            uint indexOfMin = FindIndexOfMin(array, i, (uint) (array.Length - 1));
+            SwapElements(array, i, indexOfMin);
+        }
+    }
+
 }
