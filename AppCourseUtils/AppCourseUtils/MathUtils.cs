690bdaa2b61d0472ac9345570cdd6d38b4440cc6
Marcello <marcello.faugiana@kyklos-group.com>
2024-03-25 12:58:53 +0100

    Added Utils for the course (math and strings)

---
+++ AppCourseUtils/AppCourseUtils/MathUtils.cs
@@ -0,0 +1,26 @@
+﻿namespace AppCourseUtils;
+
+public class MathUtils
+{
+    public static uint FactorialRec_v1(uint n) =>
+        // operatore ternario
+        n == 0u ? 1 : n * FactorialRec_v1(n - 1);
+
+    public static uint FactorialRec_v2(uint n)
+    {
+        if (n == 0u)
+        {
+            return 1;
+        }
+
+        return n * FactorialRec_v2(n - 1);
+    }
+
+    public static uint FactorialRec_v3(uint n) =>
+        // switch expression
+        n switch
+        {
+            0 => 1,
+            uint k => k * FactorialRec_v3(k - 1)
+        };
+}
