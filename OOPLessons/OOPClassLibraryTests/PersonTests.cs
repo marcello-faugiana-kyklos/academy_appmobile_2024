d6e7c87e99c6435e879902681c1c7b91d909f2b1
Marcello <marcello.faugiana@kyklos-group.com>
2024-03-28 13:04:13 +0100

    Fixed typo

--- OOPLessons/OOPClassLibraryTests/PersonTests.cs
+++ OOPLessons/OOPClassLibraryTests/PersonTests.cs
@@ -1,6 +1,12 @@
-﻿namespace OOPClassLibraryTests;
+﻿using OOPClassLibrary.Fiscal;
+
+namespace OOPClassLibraryTests;
 
 public class PersonTests
 {
-
+    [Fact]
+    public void PersonTest()
+    {
+        Person person = new Person();
+    }
 }
